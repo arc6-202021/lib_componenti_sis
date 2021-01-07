9a57d725fdb82ebd61e52d65ca6ba1e6c9951b04
Stefano Zenaro <mariortgasd@hotmail.com>
2021-01-07 10:58:29 +0100

    fix test sis/fsm

--- sis/setupcategory.sh
+++ sis/setupcategory.sh
@@ -3,4 +3,4 @@
 SCRIPTDIR=$(dirname $(realpath $0))
 
 # assegna stati fsm non ottimizzata per poterla testare
-sis -c "state_minimize stamina;state_assign jedi;write_blif ${SCRIPTDIR}/controllore.blif" ${SCRIPTDIR}/controllore.blif
+sis -c "state_minimize stamina;state_assign jedi;write_blif ${SCRIPTDIR}/controllore_src.blif" ${SCRIPTDIR}/controllore_src.blif
